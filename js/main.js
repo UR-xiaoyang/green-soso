f8ef7253a9f1da71fe1a91e09f317ef6542dc0f8
bilibili-xiaoyang <2422286329@qq.com>
2021-02-24 18:54:20 +0800

    upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+    $(":radio").click(function() {
+        alert("您选择了>>>>" + $(this).val());
+    });
+});
+//点击触发逻辑
+function searchToggle(obj, evt) {
+    var container = $(obj).closest('.search-wrapper');
+
+    if (!container.hasClass('active')) {
+        container.addClass('active');
+        evt.preventDefault();
+    } else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
+        container.removeClass('active');
+        // clear input
+        container.find('.search-input').val('');
+        // clear and hide result container when we press close
+        container.find('.result-container').fadeOut(100, function() {
+            $(this).empty();
+        });
+    }
+}
---
+++ js/soso.js
@@ -0,0 +1,15 @@
+$("#c-bing").click(function() {
+    $("#soso").attr("action", ["https://cn.bing.com/search"])
+});
+$("#c-google").click(function() {
+    $("#soso").attr("action", ["https://www.google.com/search"])
+});
+$("#c-yahu").click(function() {
+    $("#soso").attr("action", ["https://search.yahoo.com/search"])
+})
+$("#c-dogedoge").click(function() {
+    $("#soso").attr("action", ["https://www.dogedoge.com/results"])
+})
+$("#c-CNso").click(function() {
+    $("#soso").attr("action", ["http://www.chinaso.com/search/pagesearch.htm"])
+})
