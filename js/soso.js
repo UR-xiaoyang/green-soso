e0c7977b16f117070630263537e641542b8fdbf1
bilibili-xiaoyang <2422286329@qq.com>
2021-04-05 13:15:16 +0800

    upload

--- js/soso.js
+++ js/soso.js
@@ -1,15 +1,24 @@
 $("#c-bing").click(function() {
     $("#soso").attr("action", ["https://cn.bing.com/search"])
+    $("#search-input").attr("name", ["q"])
 });
 $("#c-google").click(function() {
     $("#soso").attr("action", ["https://www.google.com/search"])
+    $("#search-input").attr("name", ["q"])
 });
 $("#c-yahu").click(function() {
     $("#soso").attr("action", ["https://search.yahoo.com/search"])
+    $("#search-input").attr("name", ["q"])
 })
 $("#c-dogedoge").click(function() {
     $("#soso").attr("action", ["https://www.dogedoge.com/results"])
+    $("#search-input").attr("name", ["q"])
 })
 $("#c-CNso").click(function() {
     $("#soso").attr("action", ["http://www.chinaso.com/search/pagesearch.htm"])
+    $("#search-input").attr("name", ["q"])
+})
+$("#c-baidu").click(function() {
+    $("#soso").attr("action", ["http://www.baidu.com/s"])
+    $("#search-input").attr("name", ["wd"])
 })
